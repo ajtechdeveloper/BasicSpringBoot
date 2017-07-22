c0af57f3d126c974c056b99cab26a23cefb0ccdf
Aj Tech Developer <techdeveloper.aj@gmail.com>
2017-07-22 21:17:09 +0530

    Initial code commit

---
+++ src/main/java/com/aj/basicspringboot/BasicSpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.aj.basicspringboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class BasicSpringBootApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicSpringBootApplication.class);
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BasicSpringBootApplication.class,args);
+	}
+}
