00b7a609b2f318d30855b3ac29acec4952016a1d
Aj Tech Developer <techdeveloper.aj@gmail.com>
2018-10-06 10:47:07 +0530

    Upgrade Spring Boot version to 2.0.5.RELEASE

--- src/main/java/com/aj/basicspringboot/BasicSpringBootApplication.java
+++ src/main/java/com/aj/basicspringboot/BasicSpringBootApplication.java
@@ -1,17 +1,12 @@
 package com.aj.basicspringboot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicSpringBootApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(BasicSpringBootApplication.class);
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(BasicSpringBootApplication.class,args);
 	}
 }
